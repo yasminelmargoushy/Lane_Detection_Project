b21d8de4b8c387e616f3ed4a5f9c90bdd02c3a27
yasminelmargoushy <73910634+yasminelmargoushy@users.noreply.github.com>
2022-04-18 22:34:31 +0200

    Sliding Window & Line Class

--- main.py
+++ main.py
@@ -7,6 +7,105 @@ from moviepy.editor import VideoFileClip
 from collections import deque
 
 
+# Define a class to receive the characteristics of each line detection
+class Line:
+    def __init__(self):
+        # was the line detected in the last iteration?
+        self.detected = False
+
+        # x and y values in last frame
+        self.x = None
+        self.y = None
+
+        # x intercepts for average smoothing
+        self.bottom_x = deque(maxlen=frame_num)
+        self.top_x = deque(maxlen=frame_num)
+
+        # Record last x intercept
+        self.current_bottom_x = None
+        self.current_top_x = None
+
+        # Record radius of curvature
+        self.radius = None
+
+        # Polynomial coefficients: x = A*y**2 + B*y + C
+        self.A = deque(maxlen=frame_num)
+        self.B = deque(maxlen=frame_num)
+        self.C = deque(maxlen=frame_num)
+        self.fit = None
+        self.fitx = None
+        self.fity = None
+
+
+
+    def quick_sliding_window(self, nonzerox, nonzeroy, image):
+        """
+        Assuming in last frame, lane has been detected. Based on last x/y coordinates, quick search current lane.
+        """
+        x_inds = []
+        y_inds = []
+        out_img = np.dstack((image, image, image)) * 255
+        if self.detected:
+            win_bottom = 720
+            win_top = 630
+            while win_top >= 0:
+                yval = np.mean([win_top, win_bottom])
+                xval = (np.median(self.A)) * yval ** 2 + (np.median(self.B)) * yval + (np.median(self.C))
+                x_idx = np.where((((xval - 50) < nonzerox)
+                                  & (nonzerox < (xval + 50))
+                                  & ((nonzeroy > win_top) & (nonzeroy < win_bottom))))
+                x_window, y_window = nonzerox[x_idx], nonzeroy[x_idx]
+                cv2.rectangle(out_img, (int(xval - 50), win_top), (int(xval + 50), win_bottom),
+                              (0, 255, 0), 2)
+                if np.sum(x_window) != 0:
+                    np.append(x_inds, x_window)
+                    np.append(y_inds, y_window)
+                win_top -= 90
+                win_bottom -= 90
+        if np.sum(x_inds) == 0:
+            self.detected = False  # If no lane pixels were detected then perform blind search
+        return x_inds, y_inds, out_img
+
+    def blind_sliding_window(self, nonzerox, nonzeroy, image):
+        """
+        Sliding window search method, start from blank.
+        """
+        x_inds = []
+        y_inds = []
+        out_img = np.dstack((image, image, image)) * 255
+        if self.detected is False:
+            win_bottom = 720
+            win_top = 630
+            histogram_complete = np.sum(image[200:, :], axis=0)
+            while win_top >= 0:
+                histogram = np.sum(image[win_top:win_bottom, :], axis=0)
+                if self == right:
+                    base = (np.argmax(histogram[640:-60]) + 640) \
+                    if np.argmax(histogram[640:-60]) > 0\
+                    else (np.argmax(histogram_complete[640:]) + 640)
+                else:
+                    base = np.argmax(histogram[:640]) \
+                        if np.argmax(histogram[:640]) > 0 \
+                        else np.argmax(histogram_complete[:640])
+                x_idx = np.where((((base - 50) < nonzerox) & (nonzerox < (base + 50))
+                                  & ((nonzeroy > win_top) & (nonzeroy < win_bottom))))
+                x_window, y_window = nonzerox[x_idx], nonzeroy[x_idx]
+                cv2.rectangle(out_img, (int(base - 50), win_top), (int(base + 50), win_bottom),
+                              (0, 255, 0), 2)
+                if np.sum(x_window) != 0:
+                    x_inds.extend(x_window)
+                    y_inds.extend(y_window)
+                win_top -= 90
+                win_bottom -= 90
+        if np.sum(x_inds) > 0:
+            self.detected = False
+        else:
+            y_inds = self.y
+            x_inds = self.x
+        return x_inds, y_inds, out_img
+
+
+
 def draw_area(undist, left_fitx, lefty, right_fitx, righty):
     Minv = cv2.getPerspectiveTransform(dst, src)
 
@@ -34,6 +133,7 @@ def draw_area(undist, left_fitx, lefty, right_fitx, righty):
 
     # Combine the result with the original image
     return cv2.addWeighted(undist, 1, newwarp, 0.3, 0)
+
 
 
 def warp(img):
@@ -80,7 +180,6 @@ def undistort(img, mtx, dist):
     return cv2.cvtColor(dst_img, cv2.COLOR_BGR2RGB)
 
 
-
 def process_image(img):
 
     global mtx, dist, src, dst, debug
@@ -93,7 +192,20 @@ def process_image(img):
                                    b_thresh=(145, 200))
     nonzerox, nonzeroy = np.nonzero(np.transpose(warped_binary))
 
-    return warped_binary
+    if left.detected is True:
+        leftx, lefty, out_img_left = left.quick_sliding_window(nonzerox, nonzeroy, warped_binary)
+    if right.detected is True:
+        rightx, righty, out_img_right = right.quick_sliding_window(nonzerox, nonzeroy, warped_binary)
+    if left.detected is False:
+        leftx, lefty, out_img_left = left.blind_sliding_window(nonzerox, nonzeroy, warped_binary)
+    if right.detected is False:
+        rightx, righty, out_img_right = right.blind_sliding_window(nonzerox, nonzeroy, warped_binary)
+
+
+    out_combine = cv2.addWeighted(out_img_left, 1, out_img_right, 1, 0)
+
+
+    return out_combine
 
 img_shape = (720, 1280)
 img_size = [1280, 720]  # width, height
@@ -102,6 +214,8 @@ src = np.float32([[490, 482], [810, 482],
                   [1250, 720], [0, 720]])
 dst = np.float32([[0, 0], [1280, 0],
                   [1250, 720], [40, 720]])
+ploty = np.linspace(0, img_shape[0] - 1, img_shape[0])
+
 debug = True
 
 # import Camera Calibration Parameters
@@ -110,9 +224,17 @@ with open(dist_pickle, mode="rb") as f:
     CalData = pickle.load(f)
 mtx, dist = CalData["mtx"], CalData["dist"]
 
+# latest frames number of good detection
+frame_num = 15
+
+left = Line()
+right = Line()
+
 video_output = './output_videos/challenge_video_out_debug.mp4'
 input_path = './test_videos/challenge_video.mp4'
 image_name = 'test1'
+
+
 
 image_r = process_image(mpimg.imread(f'./test_images/{image_name}.jpg'))
 f, (ax1) = plt.subplots(1, 1, figsize=(20, 10))
@@ -127,5 +249,5 @@ clip1 = VideoFileClip(input_path)
 final_clip = clip1.fl_image(process_image)
 final_clip.write_videofile(video_output, audio=False)
 
-
 '''
+
